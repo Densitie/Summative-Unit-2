5ca8d411dd48f48e8b58f75985782f5140b5ff20
Densitie <55253212+Densitie@users.noreply.github.com>
2019-11-13 21:15:11 -0800

    Update Game.java

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Arun K.
+ * This program is rock, paper, scissors vs computer
+ * November 13 2019
  */
 
 package game;
